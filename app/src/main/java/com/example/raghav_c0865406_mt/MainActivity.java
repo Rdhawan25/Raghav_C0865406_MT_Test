3d4f6c7e14cf26a12ff6f7a181416d0df31cc0a1
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-12-06 05:39:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/raghav_c0865406_mt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.raghav_c0865406_mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
