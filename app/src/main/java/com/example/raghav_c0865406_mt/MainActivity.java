ed4b4480f19a2dc15b218cefe3a25045b9e1a21c
Rdhawan25 <dhawan.raghav25@gmail.com>
2022-12-06 08:27:57 +0530

    Price Implemented

--- app/src/main/java/com/example/raghav_c0865406_mt/MainActivity.java
+++ app/src/main/java/com/example/raghav_c0865406_mt/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.raghav_c0865406_mt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -16,13 +18,30 @@ import android.widget.RadioButton;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     Map<String, Integer> map = new HashMap<String, Integer>();
-    //final TextView dailyRent = findViewById(R.id.dailyRentTextView);
-    int tableNumber = 0;
+    TextView dailyRent;
+    int tableNumber = 1;
     RadioGroup rdg;
-    int age;
+    CheckBox gps;
+    CheckBox childSeat;
+    CheckBox ulMileage;
+    TextView amount;
+    TextView totalAmount;
+    int price;
+    RadioButton r1;
+    RadioButton r2;
+    RadioButton r3;
+
+
+
+
+
+    public void actionPerformed(){
+
+    }
 
 
     @Override
@@ -34,7 +53,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         final SeekBar seekBar = findViewById(R.id.seekBar);
         final TextView daysText = findViewById(R.id.daysTextView);
         rdg = findViewById(R.id.radioGroup);
+        gps = findViewById(R.id.checkBoxGPS);
+        childSeat = findViewById(R.id.checkBoxChildSeat);
+        ulMileage = findViewById(R.id.checkBoxUnlimitedMileage);
+        amount = findViewById(R.id.amoutTextView);
+        totalAmount = findViewById(R.id.totalTextView);
+        dailyRent = findViewById(R.id.dailyRentTextView);
+        r1 = findViewById(R.id.radioButtonLT20);
+        r2 = findViewById(R.id.radioButtonBTW2160);
+        r3 = findViewById(R.id.radioButtonAbove60);
 
+
+//gps.addOnAttachStateChangeListener(this);
+gps.setOnClickListener(this);
+        childSeat.setOnClickListener(this);
+        ulMileage.setOnClickListener(this);
+        rdg.setOnClickListener(this);
+        r1.setOnClickListener(this);
+        r2.setOnClickListener(this);
+        r3.setOnClickListener(this);
 
 
 
@@ -46,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         map.put("Charger", 250);
         map.put("Ferrari", 200);
         map.put("Volvo", 300);
-        map.put("Volkswagon", 120);
+        map.put("Volkswagen", 120);
         map.put("Honda", 130);
         map.put("Hyundai", 190);
 
@@ -84,24 +121,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        int price = map.get(map.keySet().toArray()[position]);
-       // final TextView dailyR = findViewById(R.id.dailyRentTextView);
-       // dailyRent.setText(String.valueOf(price));
-        int i = rdg.getCheckedRadioButtonId();
-        RadioButton r = (RadioButton) findViewById(i);
-        String rvalue = r.getText().toString();
-        System.out.println("This is Radio Output: "+ rvalue);
+        price = map.get(map.keySet().toArray()[position]);
 
-        switch (rvalue){
-            case "Less Than 20":
-                price = price+5;
-                break;
-            case "Above 60":
-                price = price-10;
-                break;
-        }
 
         System.out.println("PRice is this: "+ price);
+
+        price = price*tableNumber;
+        amount.setText(String.valueOf(price));
+        float total = (float) (price+(price*0.13));
+        totalAmount.setText(String.valueOf(total));
+
+
 
 
 
@@ -111,6 +141,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == gps){
+            price = price+5;
+        }
+        if (view == childSeat){
+            System.out.println("THIS is ** Child");
+            price = price+7;
+        }
+        if (view == ulMileage){
+            System.out.println("THIS is ** Ml");
+            price = price+10;
+        }
+        if(view == r1 || view == r3){
+            int i = rdg.getCheckedRadioButtonId();
+            RadioButton r = (RadioButton) findViewById(i);
+            String rvalue = r.getText().toString();
+            System.out.println("This is Radio Output: "+ rvalue);
+
+            switch (rvalue){
+                case "Less Than 20":
+                    System.out.println("THIS is ** Radio 1");
+                    price = price+5;
+
+                    break;
+                case "Above 60":
+                    System.out.println("THIS is ** Radio 2");
+                    price = price-10;
+                    break;
+            }
+
+        }
+
+        price = price*tableNumber;
+        amount.setText(String.valueOf(price));
+        float total = (float) (price+(price*0.13));
+        totalAmount.setText(String.valueOf(total));
 
     }
 }
